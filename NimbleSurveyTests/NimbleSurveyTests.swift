78879466aacb5faa028087089d5c2054b9b39e24
Kazu (Phu Phan) <phankyphu@gmail.com>
2024-07-31 10:39:28 +0700

    Initial Commit

---
+++ NimbleSurveyTests/NimbleSurveyTests.swift
@@ -0,0 +1,17 @@
+//
+//  NimbleSurveyTests.swift
+//  NimbleSurveyTests
+//
+//  Created by Kazu on 31/7/24.
+//
+
+import Testing
+@testable import NimbleSurvey
+
+struct NimbleSurveyTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
