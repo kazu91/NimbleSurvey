3874f6cac2976d4ad08871b972435e016d8132be
Kazu (Phu Phan) <phankyphu@gmail.com>
2024-08-01 15:55:30 +0700

    - create skeleton loading view

---
+++ NimbleSurvey/Features/SkeletonLoadingView/SkeletonLoadingHomeView.swift
@@ -0,0 +1,8 @@
+//
+//  SkeletonLoadingHomeView.swift
+//  NimbleSurvey
+//
+//  Created by Kazu on 1/8/24.
+//
+
+import Foundation
---
+++ NimbleSurvey/Utils/Views/ShimmerAnimatedView.swift
@@ -0,0 +1,8 @@
+//
+//  ShimmerAnimatedView.swift
+//  NimbleSurvey
+//
+//  Created by Kazu on 1/8/24.
+//
+
+import Foundation
